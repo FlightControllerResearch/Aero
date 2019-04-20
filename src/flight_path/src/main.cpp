3517efe7c2d75f9de7dfb61f732040320ab5e9a4
Test Account <FlightControllerResearch@gmail.com>
2019-04-20 16:23:39 -0400

    Updating test file and removing build/ and devel/ tracked folders

--- src/flight_path/src/main.cpp
+++ src/flight_path/src/main.cpp
@@ -5,14 +5,130 @@
 */
 
 #include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/CommandTOL.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
 
-int main(int argc char** argv) {
+#define FLIGHT_ALTITUDE 1.5f;
+
+mavros_msgs::State current_state;
+
+void stateCallback(const mavros_msgs::State::ConstPtr& msg) {
+	current_state = *msg;
+}
+
+
+int main(int argc, char** argv) {
 	ros::init(argc, argv, "flight_path_node");
 	ros::NodeHandle nh;
 
-	ros::Rate rate(10.0);
-	while(ros::ok() && current_state.connected) {
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, stateCallback);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    ros::ServiceClient land_client = nh.serviceClient<mavros_msgs::CommandTOL>("mavros/cmd/land");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
+	ros::Rate rate(10.0);
+	// Wait for the flight controller to connect
+	while(ros::ok() && current_state.connected) {
+		ros::spinOnce();
+		rate.sleep();
 	}
 
+	geometry_msgs::PoseStamped pose;
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 0;
+    pose.pose.position.z = FLIGHT_ALTITUDE;
+
+    //send a few setpoints before starting
+    for(int i = 100; ros::ok() && i > 0; --i){
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+
+
+	ros::Time last_request = ros::Time::now();
+
+    // change to offboard mode and arm
+    while(ros::ok() && !current_state.armed){
+        if( current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0))){
+          ROS_INFO("%s", current_state.mode.c_str());
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent){
+                ROS_INFO("Offboard enabled");
+            }
+            last_request = ros::Time::now();
+        } else {
+            if( !current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
+                    ROS_INFO("Vehicle armed");
+                }
+                last_request = ros::Time::now();
+            }
+        }
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    // go to the first waypoint
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 0;
+    pose.pose.position.z = FLIGHT_ALTITUDE;
+
+    ROS_INFO("going to the first way point");
+    for(int i = 0; ros::ok() && i < 10*20; ++i){
+      local_pos_pub.publish(pose);
+      ros::spinOnce();
+      rate.sleep();
+    }
+    ROS_INFO("first way point finished!");
+
+
+    // go to the second waypoint
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 1;
+    pose.pose.position.z = FLIGHT_ALTITUDE;
+
+    //send setpoints for 10 seconds
+    ROS_INFO("going to second way point");
+    for(int i = 0; ros::ok() && i < 10*20; ++i){
+
+      local_pos_pub.publish(pose);
+      ros::spinOnce();
+      rate.sleep();
+    }
+    ROS_INFO("second way point finished!");
+
+
+	mavros_msgs::CommandTOL land_cmd;
+    land_cmd.request.yaw = 0;
+    land_cmd.request.latitude = 0;
+    land_cmd.request.longitude = 0;
+    land_cmd.request.altitude = 0;
+
+    while (!(land_client.call(land_cmd) && land_cmd.response.success)){
+
+      ROS_INFO("Land Drone");
+      ros::spinOnce();
+      rate.sleep();
+    }
+
+
+	while(ros::ok()) {
+		rate.sleep();
+	}
+	return 0;
 }
