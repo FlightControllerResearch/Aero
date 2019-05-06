e658f6f6fe6236dbcf4afb1381d2d5826d6a903a
Luke Armbruster <larmbru@purdue.edu>
2019-05-06 17:26:07 -0400

    Removing build files

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aero/catkin_ws/src
-# Build directory: /home/aero/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(flight_path)
-subdirs(ex1)
