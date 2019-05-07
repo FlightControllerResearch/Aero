884db4ae4c1d5ec9c103bce58876c114db6df9a0
Test Account <FlightControllerResearch@gmail.com>
2019-05-07 11:42:58 -0400

    Fixing ntrip client and updating firebase manager

--- src/fb/scripts/fb_manager.py
+++ src/fb/scripts/fb_manager.py
@@ -8,7 +8,7 @@ from sensor_msgs.msg import NavSatFix
 from rtk.msg import HeadingSpeed
 import time
 
-ref = firebase.FirebaseApplication('https://sd-d2u.firebaseio.com', None)
+ref = firebase.FirebaseApplication('https://sd-d2u.firebaseio.com', None) # public firebase 
 
 def gpsCallback(data):
 	ref.put('', 'Current Position', str(data.latitude) + ',' + str(data.longitude) + ',' + str(data.altitude) + ',' + str(data.status.status) + ',' + str(data.status.service))
@@ -19,22 +19,22 @@ def controllerCallback(data):
 def listener():
 	rospy.init_node('fb_listener', anonymous=True)
 	rospy.Subscriber("rtk_gpgga", NavSatFix, gpsCallback)
-	delivery_status_pub = rospy.Publisher('delivery_requested_status', Bool, queue_size=1)
-	prev_delivery_requested_status = False
-	delivery_requested_status = False
+	flight_status_pub = rospy.Publisher('flight_requested_status', Bool, queue_size=1)
+	prev_flight_requested_status = False
+	flight_requested_status = False
 	while not rospy.core.is_shutdown():
 		rospy.sleep(0.5)
 		try:
-			delivery_requested_status = ref.get('/Delivery Requested', None)
+			flight_requested_status = ref.get('/Flight Requested', None)
 		except: 
 			rospy.loginfo("Not connected to database")
 
-		if delivery_requested_status != prev_delivery_requested_status:
-			rospy.loginfo("Delivery information changed")
-			delivery_status_msg = Bool()
-			delivery_status_msg.data = delivery_requested_status
-			delivery_status_pub.publish(delivery_status_msg)
-			prev_delivery_requested_status = delivery_requested_status
+		if flight_requested_status != prev_flight_requested_status:
+			rospy.loginfo("Flight information changed")
+			flight_status_msg = Bool()
+			flight_status_msg.data = flight_requested_status
+			flight_status_pub.publish(flight_status_msg)
+			prev_flight_requested_status = flight_requested_status
 
 if __name__ == '__main__':
   listener()
